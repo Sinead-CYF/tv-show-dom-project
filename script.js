f0410548e52f0f13d10959fe80416c7bf5173175
Sinead <77743497+Sinead-CYF@users.noreply.github.com>
2021-10-24 15:28:51 +0100

    TV-Maze Link & CSS Styles added.

--- script.js
+++ script.js
@@ -11,51 +11,72 @@ body.appendChild(titleWrap);
 titleWrap.appendChild(header);
 
 function makePageForEpisodes(episodeList) {
-  //EPISODE COUNT BANNER
+
+  //EPISODE COUNT WRAPPER
   const episodeCountWrap = document.createElement("div");
   episodeCountWrap.classList.add("episode-count-wrapper");
-  const episodeCountPara = document.createElement("p");
-  episodeCountPara.textContent = `${episodeList.length} episode(s)`;
   body.appendChild(episodeCountWrap);
+
+  //EPISODE COUNT PARAGRAPH 
+  const episodeCountPara = document.createElement("p");
+  episodeCountPara.classList.add("episode-count-para");
+  episodeCountPara.innerHTML= `${episodeList.length} Episodes`;
   episodeCountWrap.appendChild(episodeCountPara);
+
+  //TV-MAZE LINK 
+  const tvMazeLink = document.createElement("a");
+  tvMazeLink.classList.add("tv-maze-link");
+  tvMazeLink.innerHTML = "SOURCED FROM TV-MAZE.COM"
+  tvMazeLink.href = "https://www.tvmaze.com";
+  episodeCountWrap.appendChild(tvMazeLink);
 
   //TILE-AREA WRAPPER
   const cardsWrapper = document.createElement("div");
   body.appendChild(cardsWrapper);
   cardsWrapper.classList.add("cards-wrapper");
 
-  episodeList.forEach(episode => {
+  episodeList.forEach((episode) => {
+    //CREATING AN EPISODE CARD
+    const card = document.createElement("div");
+    card.classList.add("card");
+    cardsWrapper.appendChild(card);
 
-  //CREATING AN EPISODE CARD
-  const card = document.createElement("div");
-  card.classList.add("card");
-  cardsWrapper.appendChild(card);
+    //CARD IMG WRAPPER
+    const imgWrapper = document.createElement("div");
+    imgWrapper.classList.add("img-wrapper");
+    card.appendChild(imgWrapper);
 
-  //CARD IMG WRAPPER
-  const imgWrapper = document.createElement("div");
-  imgWrapper.classList.add("img-wrapper");
-  card.appendChild(imgWrapper);
+    //IMAGES
+    const images = document.createElement("img");
+    images.classList.add("images");
+    images.src = episode.image.medium;
+    imgWrapper.appendChild(images);
 
-  //IMAGES
-  const images = document.createElement("img");
-  images.classList.add("images");
-  images.src = episode.image.medium;
-  imgWrapper.appendChild(images);
+    //CAPTURING SEASON & EPISODE DETAILS
+    let seasonNum = episode.season;
+    let episodeNum = episode.number;
+    
+    //ZERO-PADDING NUMBERS
+    if(seasonNum < 10){
+      seasonNum = `0${seasonNum}`;
+    }
 
-  //CARD HEADER
-  const cardHeader = document.createElement("h2");
-  cardHeader.classList.add("card-header");
-  cardHeader.textContent = `${episode.name} - S1E12`;
-  card.appendChild(cardHeader);
+    if (episodeNum < 10){
+      episodeNum = `0${episodeNum}`;
+    }
 
-  //CARD PARAGRAPH
-  const cardParagraph = document.createElement("div");
-  cardParagraph.classList.add("card-paragraph-wrapper");
-  cardParagraph.innerHTML = episode.summary;
-  card.appendChild(cardParagraph);
+    //CARD HEADER
+    const cardHeader = document.createElement("h2");
+    cardHeader.classList.add("card-header");
+    cardHeader.textContent = `${episode.name} - S${seasonNum}${episodeNum}`;
+    card.appendChild(cardHeader);
+
+    //CARD PARAGRAPH
+    const cardParagraph = document.createElement("div");
+    cardParagraph.classList.add("card-paragraph-wrapper");
+    cardParagraph.innerHTML = episode.summary;
+    card.appendChild(cardParagraph);
   });
-
-  
 }
 
 function setup() {
@@ -69,19 +90,20 @@ window.onload = setup;
 
 *** Level 100 - minimal features: ***
 
-All episodes must be shown
+All episodes must be shown - DONE 
 For each episode, AT LEAST following must be displayed:
 
-the episode's name
-the season number
-the episode number
-the episode's medium-sized image
-the episode's summary text
+the episode's name -- DONE
+the season number  --  DONE
+the episode number  -- DONE
+the episode's medium-sized image -- DONE
+the episode's summary text -- DONE
 
 You should combine season number and episode number into an episode code:
 Each part should be zero-padded to two digits.
 Example: S02E07 would be the code for the 7th episode of the 2nd season. 
-S2E7 would be incorrect.
+S2E7 would be incorrect. -- DONE
+
 Your page should state somewhere that the data has 
 (originally) come from TVMaze.com, and link back to that site 
 (or the specific episode on that site). See tvmaze.com/api#licensing. 
