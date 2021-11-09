87f803996904ccb959c7ee0203bcb0e03d4ade78
Sinead <77743497+Sinead-CYF@users.noreply.github.com>
2021-11-09 13:43:38 +0000

    Show Cards CSS Update

--- script.js
+++ script.js
@@ -247,31 +247,44 @@ function showCards(shows) {
   shows.forEach((show) => {
     //CREATING AN SHOW CARD
     const card = document.createElement("div");
-    card.classList.add("card");
+    card.classList.add("show-card");
     cardsWrapper.appendChild(card);
 
     //CARD IMG WRAPPER
     const imgWrapper = document.createElement("div");
-    imgWrapper.classList.add("img-wrapper");
+    imgWrapper.classList.add("show-img-wrapper");
     card.appendChild(imgWrapper);
 
     //IMAGES
     const images = document.createElement("img");
-    images.classList.add("images");
-    images.src = show.image.original;
+    images.classList.add("show-images");
+    images.src = show.image.medium;
     imgWrapper.appendChild(images);
+
+    //TITLE & PARAGRAPH WRAPPER
+    const showTextWrapper = document.createElement("div");
+    showTextWrapper.classList.add("show-text-wrapper");
+    card.appendChild(showTextWrapper);
 
     //CARD HEADER
     const cardHeader = document.createElement("h2");
-    cardHeader.classList.add("card-header");
+    cardHeader.classList.add("show-card-header");
     cardHeader.textContent = `${show.name}`;
-    card.appendChild(cardHeader);
+    showTextWrapper.appendChild(cardHeader);
 
     //CARD PARAGRAPH
     const cardParagraph = document.createElement("p");
-    cardParagraph.classList.add("card-paragraph-wrapper");
+    cardParagraph.classList.add("show-card-paragraph-wrapper");
     cardParagraph.innerHTML = show.summary;
-    card.appendChild(cardParagraph);
+    showTextWrapper.appendChild(cardParagraph);
+
+    //SHOW LINK
+    const showLink = document.createElement("a");
+    showLink.classList.add("show-link");
+    showLink.href = show.url;
+    showLink.setAttribute("target", "_blank");
+    showLink.textContent= "Read More ...";
+    showTextWrapper.appendChild(showLink);
   });
 }
 
